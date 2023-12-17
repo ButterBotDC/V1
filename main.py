e0a27d4903f7025ab085073e1f253b6307eded1c
DieMeister <135468820+DieMeister@users.noreply.github.com>
2023-12-17 12:25:46 +0100

    chnaged commands
    deleted /test
    added /code

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ async def on_member_join(member):
     await channel.send(f"Hallo <@{member.id}>, wilkommen im ButterflyHub. Um dir selbst Game-Roles zu geben schau gerne in 'Channels & Roles' (ganz oben in der Seitenleiste) vorbei. Wenn du Fragen oder Probleme hast zieh gerne ein Ticket in <#1079887914869194762>. Ansonsten viel Spaß auf dem Server :Butterfly_butterfly:.")
 
 
-@client.slash_command(name = "test", description = "Testcommand um Zeugs zu testen", guild_ids=[server_id])
-async def test(interaction: Interaction):
-    await interaction.response.send_message("Nur ein Test :)")
+@client.slash_command(name = "code", description = "Link zum GitHub Repository", guild_ids=[server_id])
+async def code(interaction: Interaction):
+    await interaction.response.send_message("https://github.com/DieMeister/ButterBot")
 
 
 client.run(bot_token)
